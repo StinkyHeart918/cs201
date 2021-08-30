4de74b4473c9d01a12e155cedb5f549759086cf4
WilliamO <willyodom17@gmail.com>
2021-08-30 02:01:44 -0800

    Added lines for the poem, including formatting. Project complete.

--- HW0/poem/poem.cpp
+++ HW0/poem/poem.cpp
@@ -4,5 +4,15 @@ using std::endl;
 
 int main()
 {
+	cout << "Time Is" << endl;
+	cout << "By Henry van Dyke" << endl;
+	cout << "" << endl;
+	cout << "Time is" << endl;
+	cout << "Too Slow for those who Wait," << endl;
+	cout << "Too Swift for those who Fear," << endl;
+	cout << "Too Long for those who Grieve," << endl;
+	cout << "Too Short for those who Rejoice;" << endl;
+	cout << "But for those who Love," << endl;
+	cout << "Time is not." << endl;	/*This is the whole poem*/
 	return 0;
 }
