671a3d170fde44e1bf32cd2fdca0101bfcf657d3
WilliamO <willyodom17@gmail.com>
2021-09-01 00:44:48 -0800

    Finalized text for game.cpp project.

--- HW0/game/game.cpp
+++ HW0/game/game.cpp
@@ -1,7 +1,7 @@
 /*
 Hello.cpp
 William Odom Jr.
-8/30/2021
+9/1/2021
 Game program for HW0
 */
 
@@ -11,6 +11,11 @@ using std::endl;
 
 int main()
 {
-	cout << "I enjoy the board game Catan." << endl; 
+	cout << "I enjoy the board game Catan. It's a strategy game where the objective is to" << endl; 
+	cout << "collect resourece to trade, build settlements, and earn points to win. The winning score is" << endl; 
+	cout << "10 points. The game is built to have 4 players, but has expansions to allow for more. " << endl; 
+	cout << "The board is fairly simple with a number of hexagonal panels place in a random order within" << endl;
+	cout << "the outlined structure. I have had many good memories staying up late playing the game and " << endl; 
+	cout << "have found it to be a great way to spend time with family and friends. " << endl; 
 	return 0; 
 }
