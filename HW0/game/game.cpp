1841c166771d6998d871a7f9545fd6d5486ebb71
WilliamO <willyodom17@gmail.com>
2021-09-01 00:33:20 -0800

    Added basic function to main of game.cpp.

--- HW0/game/game.cpp
+++ HW0/game/game.cpp
@@ -2,14 +2,15 @@
 Hello.cpp
 William Odom Jr.
 8/30/2021
-Hello world program for HW0
+Game program for HW0
 */
 
 #include <iostream>
 using std::cout; 
 using std::endl; 
 
-main()
+int main()
 {
-
+	cout << "I enjoy the board game Catan." << endl; 
+	return 0; 
 }
---
+++ labs/L01/L01.cpp
@@ -0,0 +1,17 @@
+/*
+Hello.cpp
+William Odom Jr.
+8/30/2021
+L01 program for Lab 1
+*/
+
+#include <iostream> 
+using std::cin;
+using std::cout; 
+using std::endl; 
+
+int main()
+{ 
+	cout << "Hello, World!!" << endl;
+	return 0; 
+}
