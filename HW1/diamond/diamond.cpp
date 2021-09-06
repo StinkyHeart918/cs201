5e320bc3939db16aa94546d97aee3c4fa0d500bf
WilliamO <willyodom17@gmail.com>
2021-09-06 07:01:19 -0800

    Created Diamond project and added main + intro information

---
+++ HW1/diamond/diamond.cpp
@@ -0,0 +1,16 @@
+/*
+diamond.cpp
+William Odom
+9/6/2021
+Program to take a number from the user and make a diamond of that size. 
+*/
+
+#include <iostream>
+using std::cin; 
+using std::cout;
+using std::endl; 
+
+int main()
+{
+
+}
