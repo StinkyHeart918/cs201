65ba0252a28a822a6bcfcc17c3cf623ec14eaedc
WilliamO <willyodom17@gmail.com>
2021-09-08 10:03:18 -0800

    Added to main, now accepts values associated to vectors A and B, but only as integers still.  No real output yet.

--- HW1/dotcross/dotcross.cpp
+++ HW1/dotcross/dotcross.cpp
@@ -5,3 +5,29 @@ William Odom Jr.
 Program that take a user input for vectors, then outputs the dot product and cross product. 
 */
 
+#include <iostream>
+using std::cin; 
+using std::cout; 
+using std::endl;
+
+int main()
+{
+	int Ax = 0; int Ay = 0; int Az = 0; int Bx = 0; int By = 0; int Bz = 0; //make these floats when you confirm how
+	
+	cout << "Please enter the 3D vectors you want to dot and cross multiply starting with vector A: " << endl;
+	cout << "X = "; 
+	cin >> Ax;
+	cout << "Y = ";
+	cin >> Ay;
+	cout << "Z = ";
+	cin >> Az; 
+	cout << "Now for vector B: " << endl;
+	cout << "X = ";
+	cin >> Bx;
+	cout << "Y = ";
+	cin >> By;
+	cout << "Z = ";
+	cin >> Bz;
+	cout << "A dot B = " << endl;
+	cout << "A cross B = ";
+}
