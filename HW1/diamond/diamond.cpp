c40c15f5f8ee94807c1a2728d9a35831ddaae869
WilliamO <willyodom17@gmail.com>
2021-09-08 08:43:22 -0800

    Almost have the whole diamond generated as desired. Working on the last nested while loop.

--- HW1/diamond/diamond.cpp
+++ HW1/diamond/diamond.cpp
@@ -12,36 +12,61 @@ using std::endl;
 
 int main()
 {
-	int i = 0; int j = 1; int counter; int counter2 = 0; int length;
+	int i = 0; int j = 1; int counterfront = 1; int counterback = 0; int length;
 	cout << "Please enter the number for the size of diamond you would like to be made: ";
 	cin >> length; 
-	if (length > 0)
+	if (length > 0) //Verify that it is in fact a number entered
 	{
-		counter = 1; 
-		while (i < length)
+		while (i < length) //This while loop makes the top half of the diamond successfully
 		{
 			while (j < length)
 			{
 				cout << " ";
 				j++;
 			}
-			while (counter > 0)
+			while (counterfront > 0)
 			{
 				cout << "#";
-				counter--;
+				counterfront--;
 			}
-			while (counter2 > 0)
+			while (counterback > 0)
 			{
 				cout << "#";
-				counter2--;
+				counterback--;
 			} 
 			cout << endl;
 			i++;
-			counter = i + 1;
-			counter2 = i; 
+			counterfront = i + 1;
+			counterback = i; 
 			j = i + 1; 
 		}
-
+		i = length - 2; 
+		j = length - 1; 
+		counterfront = i + 1; 
+		counterback = i; 
+		while (i > 0)
+		{
+			while (j < length)
+			{
+				cout << " ";
+				j++;
+			}
+			while (counterfront > 0)
+			{
+				cout << "#";
+				counterfront--;
+			}
+			while (counterback > 0)
+			{
+				cout << "#";
+				counterback--;
+			}
+			cout << endl;
+			i--;
+			counterfront = i + 1;
+			counterback = i;
+			j = i + 1;
+		}
 		return length; 
 	}
 	else //In case someone enters an invalid character or number. 
