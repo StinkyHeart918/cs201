dcbcae3a447ea5d8e1cc66a6bf71688af5aa1ece
WilliamO <willyodom17@gmail.com>
2021-09-08 11:43:27 -0800

    Finished program dotcross.cpp. Added comments to clarify code.

--- HW1/dotcross/dotcross.cpp
+++ HW1/dotcross/dotcross.cpp
@@ -14,8 +14,8 @@ using std::endl;
 int main()
 {
 	std::setprecision(5);
-	double Ax = 0; double Ay = 0; double Az = 0; double Bx = 0; double By = 0; double Bz = 0; 
-	double Cx = 0; double Cy = 0; double Cz = 0; double dot = 0; 
+	double Ax = 0; double Ay = 0; double Az = 0; double Bx = 0; double By = 0; double Bz = 0; //Input from user
+	double Cx = 0; double Cy = 0; double Cz = 0; double dot = 0; //Caluculated output
 	cout << "Please enter the 3D vectors you want to dot and cross multiply starting with vector A: " << endl;
 	cout << "X = "; 
 	cin >> Ax;
@@ -30,6 +30,10 @@ int main()
 	cin >> By;
 	cout << "Z = ";
 	cin >> Bz;
+	dot = Ax * Bx + Ay * By + Az * Bz; //Dot product formula, no paranthesis needed due to order of opperations
+	Cx = Ay * Bz - Az * By; //Cross product for x direction, subsequent lines for y and z. 
+	Cy = Az * Bx - Ax * Bz;
+	Cz = Ax * By - Ay * Bx; 
 	cout << "A dot B = " << dot << endl;
 	cout << "A cross B = <" << Cx << ", " << Cy << ", " << Cz << ">";
 }
