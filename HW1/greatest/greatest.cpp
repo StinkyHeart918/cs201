cd405ebfe4123ad8dc66999cb0f4b09c5d7d82a2
WilliamO <willyodom17@gmail.com>
2021-09-08 09:19:45 -0800

    Gave structure to greatest.cpp, potentially all the necessary key pieces to complete project.

--- HW1/greatest/greatest.cpp
+++ HW1/greatest/greatest.cpp
@@ -4,3 +4,28 @@ William Odom
 9/8/2021
 This program will take integers until a 0 is entered, at which point it spits out the biggest int entered.
 */
+
+#include <iostream>
+using std::cin; 
+using std::cout; 
+using std::endl;
+
+int main()
+{
+	int temp = 1; int great = 1;
+	cout << "Please enter a sequence of positive integers, ending the sequence with a 0. This program will print out the greatest integer entered." << endl; 
+	while (temp > 0)
+	{
+		cout << "Please enter an integer: ";
+		cin >> temp;
+		if (temp >= 0)
+		{
+			continue;
+		}
+		else
+		{
+			break; 
+		}
+	}
+	
+}
