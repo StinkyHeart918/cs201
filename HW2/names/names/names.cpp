7e49796db7f0dd8098d7a53242eb8aa393c0ee9f
WilliamO <willyodom17@gmail.com>
2021-09-18 02:07:05 -0800

    Basic idea for accepting names and outputing names made. NOT in functions yet.

--- HW2/names/names/names.cpp
+++ HW2/names/names/names.cpp
@@ -15,10 +15,22 @@ using std::cin;
 using std::endl; 
 using std::vector; 
 using std::string; 
+using std::getline;
 
 int main()
 {
-
+	vector<string> names; 
+	for (int i = 0; i < 10; i++) //accepts names from the user
+	{
+		string name; 
+		cout << "Please enter a name." << endl;
+		getline(cin, name);
+		names.push_back(name);
+	}
+	for (string name : names) //print, in order of entering, the names entered
+	{
+		std::cout << name << ", ";
+	}
 }
 
 //void InputNames(vector<string>& names)
