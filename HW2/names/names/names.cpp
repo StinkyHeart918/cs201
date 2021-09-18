06ced658ea777d5635d54c1caa99e9c9b6fb1f7c
WilliamO <willyodom17@gmail.com>
2021-09-18 01:46:58 -0800

    Created files for names.cpp. Added the required functions, without any code inside them, for structure.

---
+++ HW2/names/names/names.cpp
@@ -0,0 +1,38 @@
+/*
+names.cpp
+William Odom Jr. 
+9/18/2021
+Uses several functions to input names from the user and does... something? 
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::cout;
+using std::cin; 
+using std::endl; 
+using std::vector; 
+using std::string; 
+
+int main()
+{
+
+}
+
+//void InputNames(vector<string>& names)
+//{
+
+//}
+
+//bool DoesNameExist(const string& nameToFind, const vector<string>& names)
+//{
+
+//}
+
+//void PrintNames(const vector<string> & names)
+//{
+
+//}
+
