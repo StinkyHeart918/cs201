a703ae1108b4ef956edfc7a15deb5410d33c3a7d
WilliamO <willyodom17@gmail.com>
2021-09-20 10:10:38 -0800

    I'm trying to understand and get a real usable value to be returned from DoesNameExist.

--- HW2/names/names/names.cpp
+++ HW2/names/names/names.cpp
@@ -17,32 +17,44 @@ using std::vector;
 using std::string; 
 using std::getline;
 
-bool DoesNameExist(const string& nameToFind, const vector<string>& names)
+bool DoesNameExist(const string & nameToFind, const vector<string> & names) //Checks if the name already exists within our list of names. 
 {
-	std::find(names.begin(), names.end(), "nameToFind");
-	return 1;
+	if (std::find(begin(names), end(names), "nameToFind") != std::end(names))
+		return false; //name does exist
+	else
+		return true; // name doesn't exists
 }
 
 void InputNames(vector<string> & names)  //accepts names from the user
 {
 	for (int i = 0; i < 10; i++) 
 	{
-		string name;
-		cout << "Please enter a name." << endl;
-		getline(cin, name);
-		if (DoesNameExist(name, names) == 0)
-		{
-			cout << "You used that name already!" << endl; 
-		}
+		string name = "bbbbbb";
+//		while (DoesNameExist(name, names) != true)
+//		{
+			cout << "Please enter a name." << endl;
+			getline(cin, name);
+//			if(DoesNameExist(name, names) == true)
+//				cout << "You've already used that name!" << endl;
+//		}
 		names.push_back(name);
 	}
 }
 
-void PrintNames(const vector<string> & names) //print, in order of entering, the names entered
+//void PrintNames(const vector<string> & names) //print, in order of entering, the names entered
+//{
+	//for (string name : names) 
+	//{
+	//	std::cout << name << ", ";
+	//}
+//}
+
+void PrintNamesInteresting(const vector<string> & names) //prints the names and their respective size
 {
-	for (string name : names) 
+	for (string name : names)
 	{
-		std::cout << name << ", ";
+		cout << "The name " << name << " is ";
+		cout << size(name) << " characters long including spaces! " << endl;
 	}
 }
 
@@ -50,5 +62,5 @@ int main()
 {
 	vector<string> names;
 	InputNames(names);
-	PrintNames(names);
+	PrintNamesInteresting(names);
 }
