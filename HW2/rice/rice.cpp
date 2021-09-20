40d4a11b8233f07f5af401256ad94385855ef49e
WilliamO <willyodom17@gmail.com>
2021-09-20 03:54:31 -0800

    Program now runs and shows number of rice required to break the 1000, 1000000, and 1000000000 threshold.

--- HW2/rice/rice.cpp
+++ HW2/rice/rice.cpp
@@ -12,9 +12,41 @@ using std::endl;
 
 int main()
 {
+	int i = 0; 
 	int tile = 1; int rice = 1; int total = 1;
-	double til = 1; double ric = 1; double tot = 1;
-
-	cout << "Rice on tile " << tile << " is " << rice << " and in total there are " << total << endl;
+	double til = 1; double ric = 1; double tot = 1; 
+	cout << "The number of rice on tile " << tile << " is " << rice << " and in total there are " << total << endl;
+	//while (i < 64) //Loop for all rice tiles
+	//{
+	//	tile++; 
+	//	rice = rice * 2; 
+	//	total = total + rice;
+	//	i++;
+	//	cout << "The number of rice on tile " << tile << " is " << rice << " and in total there are " << total << endl;
+	//} 
+	while (total < 1000) //Loop for getting rice totalling 1000 or more: This gives a total of 10 tiles needed
+	{
+		tile++;
+		rice = rice * 2;
+		total = total + rice;
+		cout << "The number of rice on tile " << tile << " is " << rice << " and in total there are " << total << endl;
+	}
+	cout << "You need " << tile << " tiles to get more than 1000 grains of rice" << endl;
+	while (total < 1000000) //Loop for getting rice totalling 1000000 or more: This gives a total of 20 tiles needed
+	{
+		tile++;
+		rice = rice * 2;
+		total = total + rice;
+		cout << "The number of rice on tile " << tile << " is " << rice << " and in total there are " << total << endl;
+	}
+	cout << "You need " << tile << " tiles to get more than 1000000 grains of rice" << endl;
+	while (total < 1000000000) //Loop for getting rice totalling 1000000000 or more: This gives a total of 30 tiles needed
+	{
+		tile++;
+		rice = rice * 2;
+		total = total + rice;
+		cout << "The number of rice on tile " << tile << " is " << rice << " and in total there are " << total << endl;
+	}
+	cout << "You need " << tile << " tiles to get more than 1000000000 grains of rice" << endl;
 	return 0;
 }
