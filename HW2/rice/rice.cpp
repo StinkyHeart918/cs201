8f53cc605bf4070bdf2275d3bd5008909f8e0346
WilliamO <willyodom17@gmail.com>
2021-09-20 03:24:08 -0800

    Set up program rice.cpp with structure.

---
+++ HW2/rice/rice.cpp
@@ -0,0 +1,20 @@
+/*
+rice.cpp
+William Odom Jr
+9/19/2021
+Program calculates amount of rice owed to the inventor of chess for whatever tile he is one and total
+*/
+
+#include <iostream>
+
+using std::cout; 
+using std::endl; 
+
+int main()
+{
+	int tile = 1; int rice = 1; int total = 1;
+	double til = 1; double ric = 1; double tot = 1;
+
+	cout << "Rice on tile " << tile << " is " << rice << " and in total there are " << total << endl;
+	return 0;
+}
