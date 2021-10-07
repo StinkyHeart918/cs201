4af45eea16be8411f71433809b6d6f0bdae1738d
WilliamO <willyodom17@gmail.com>
2021-10-07 02:24:37 -0800

    Began preparing the ReadLine function. Should work as desired.

--- HW4/tokenizer/tokenizer.cpp
+++ HW4/tokenizer/tokenizer.cpp
@@ -10,7 +10,16 @@ William Odom Jr.
 #include <string>
 #include "tokenizer.hpp"
 
+using std::cout;
+using std::endl;
+using std::string; 
+using std::vector;
+
 int main()
 {
-
+	string str; 
+	vector<string> token; 
+	cout << "Please enter some text. Once you finish, type 'end', 'END', or 'End' to stop the program: " << endl;
+	ReadLine(str);
+	cout << str;
 }
--- HW4/tokenizer/tokenizertest.cpp
+++ HW4/tokenizer/tokenizertest.cpp
@@ -10,14 +10,23 @@ William Odom Jr.
 #include <string>
 #include "tokenizer.hpp"
 
+using std::cin; 
+using std::getline; 
+using std::istringstream; 
+
 bool ReadLine(std::string& str)
 {
-
+	str = "";
+	getline(cin,str);
+	if (str != "")
+		return true;
+	else
+		return false; 
 }
 
 unsigned StringToTokensWS(const std::string& input, std::vector<std::string>& tokens)
 {
-
+	return 0; 
 }
 
 void AnalyzeTokens(std::vector<std::string>& tokens)
