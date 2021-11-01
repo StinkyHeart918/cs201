d6eddc2ae69a7a74daa626e62d04689855eb0ab6
WilliamO <willyodom17@gmail.com>
2021-10-31 20:21:28 -0800

    book-db.cpp mostly structured. Can handle user inputs for integers to get to appropriate function.

--- HW5/book-db/book-db.cpp
+++ HW5/book-db/book-db.cpp
@@ -23,7 +23,8 @@ struct Book {
 	string ISBN; 
 	string Title; 
 	string Author;
-	int Year; 
+	int Year;
+	bool Loan;
 };
 
 void List(const vector<Book> & MyBooks)
@@ -33,24 +34,72 @@ void List(const vector<Book> & MyBooks)
 
 bool Loaned(const vector<Book>& MyBooks)
 {
-
+	return true; 
 }
 
 bool NotLoaned(const vector<Book>& MyBooks)
 {
-
+	return true; 
 }
 
-bool SearchBookISBN(const vector<Book>& MyBooks)
+bool SearchISBN(const vector<Book>& MyBooks)
 {
-
+	return true; 
 }
 
 int main()
 {
 	vector<Book> MyBooks{
-		{"90099090990", "Bobert's Bob", "Bob Bob Bob", 1990},
-		{"80099090990", "Cobert's Bob", "Cob Bob Bob", 1991}
+		{"9009909099-0", "Bobert's Bob", "Bob Bob Bob", 1990, true},
+		{"8009909099-0", "Cobert's Bob", "Cob Bob Bob", 1991, false}
 	};
+	bool working = true; 
+	int input; 
+	int input2;
+	string searchFor = "";
+	int year = 0000; 
 
+
+	cout << "Hello!" << endl;
+	while (working)
+	{
+		cout << "Please select an action by typing a number." << endl;
+		cout << "1: List Books in Collection" << endl;
+		cout << "2: List Books in Loaned Out" << endl;
+		cout << "3: List Books in Books Not Loaned Out" << endl;
+		cout << "4: Search for a Book" << endl;
+		cout << "5: End Program" << endl;
+		cin >> input; 
+		if (input == 1)
+			List(MyBooks);
+		if (input == 2)
+			Loaned(MyBooks);
+		if (input == 3)
+			NotLoaned(MyBooks);
+
+		if (input == 4)
+		{
+			//Handles what method the user wants to search with
+			while (working)
+			{
+				cout << "How would you like to look for this book?" << endl;
+				cout << "1: Search by ISBN" << endl;
+				cout << "2: Search by Title" << endl;
+				cout << "3: Search by Author" << endl;
+				cout << "4: Search by Year" << endl;
+				cout << "5:  Go Back" << endl;
+				cin >> input2; 
+				if (input2 == 1)
+					SearchISBN(MyBooks);
+				if (input2 == 5)
+					break;
+				else 
+					cout << "Oops! That's not a valid input. Please try again. " << endl;
+			}
+		}
+		if (input == 5)
+			working = false; 
+		else
+			cout << "Oops! That's not a valid input. Please try again. " << endl;
+	}
 }
