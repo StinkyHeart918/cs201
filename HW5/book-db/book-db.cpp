6b9cabfc4a1649c41047a2143029a650219bc389
WilliamO <willyodom17@gmail.com>
2021-11-01 05:34:16 -0800

    Completed book-db.cpp project.

--- HW5/book-db/book-db.cpp
+++ HW5/book-db/book-db.cpp
@@ -28,16 +28,22 @@ struct Book {
 	bool Loan;
 };
 
+/*
+Prints out all book in order that they are stored within the vector
+*/
 void List(const vector<Book> & MyBooks)
 {
 	ostringstream oss1;
-	oss1 << endl << "Your loaned books are: " << endl;
+	oss1 << endl << "Your whole collection of books is: " << endl;
 	for (auto book : MyBooks)
 		oss1 << book.Title << ", " << book.Author << ", " << book.Year << ", " << book.ISBN << endl;
 	oss1 << endl;
 	cout << oss1.str();
 }
 
+/*
+Prints out all book classified as loaned
+*/
 void Loaned(const vector<Book>& MyBooks)
 {
 	ostringstream ossl; 
@@ -49,6 +55,9 @@ void Loaned(const vector<Book>& MyBooks)
 	cout << ossl.str();
 }
 
+/*
+Prints out all book classified as not loaned
+*/
 void NotLoaned(const vector<Book>& MyBooks)
 {
 	ostringstream ossnl; 
@@ -60,47 +69,35 @@ void NotLoaned(const vector<Book>& MyBooks)
 	cout << ossnl.str();
 }
 
+/*
+This functino does the searching, and if there are multiple books which satisfy a desired condition 
+will print all the apply. 
+*/
 void SearchStr(const vector<Book>& MyBooks, string target, int type)
 {
 	ostringstream osssearch; 
 	for (auto book : MyBooks)
 	{
 		if (type == 1)
-		{
 			if (book.ISBN == target)
-			{
 				osssearch << book.Title << ", " << book.Author << ", " << book.Year << ", " << book.ISBN << endl;
-				osssearch << endl;
-			}
-		}
 		if (type == 2)
-		{
 			if (book.Title == target)
-			{
 				osssearch << book.Title << ", " << book.Author << ", " << book.Year << ", " << book.ISBN << endl;
-				osssearch << endl;
-			}
-		}
 		if (type == 3)
-		{
 			if (book.Author == target)
-			{
 				osssearch << book.Title << ", " << book.Author << ", " << book.Year << ", " << book.ISBN << endl;
-				osssearch << endl;
-			}
-		}
 		if(type == 4)
-		{
 			if (book.Year == target)
-			{
 				osssearch << book.Title << ", " << book.Author << ", " << book.Year << ", " << book.ISBN << endl;
-				osssearch << endl;
-			}
-		}
 	}
+	osssearch << endl;
 	cout << osssearch.str();
 }
 
+/*
+Prompt user to determine what type of term they want to search with and what the search is for
+*/
 void SearchPrompt(const vector<Book>& MyBooks)
 {
 	int input;
@@ -115,7 +112,9 @@ void SearchPrompt(const vector<Book>& MyBooks)
 		cout << "4: Search by Year" << endl;
 		cout << "5: Go Back" << endl;
 		cin >> input;
-		getline(cin, target); //Why do I need this one here for it to work where I want it too? 
+		//Why do I need this one (below) for the program to properly work where I want it too? I want the 
+		//getline to occur AFTER saying what search we are prompting for. 
+		getline(cin, target); 
 		if (input > 0 && input < 5)
 		{
 			if (input == 1)
@@ -139,11 +138,33 @@ void SearchPrompt(const vector<Book>& MyBooks)
 	}
 }
 
+/*
+Sets up our vector of books, for purpose of this assignment, and then prompts user for
+commands on what to do. 
+*/
 int main()
 {
-	vector<Book> MyBooks{
-		{"Bobert's Bob", "Bob Bob Bob", "1990", "9009909099-0", true},
-		{"Cobert's Bob", "Cob Bob Bob", "1991", "8009909099-0", false}
+	vector<Book> MyBooks{//First 4 are a real set of books, the rest are made up for this project
+		{"Eragon", "Christopher Paolini", "2002", "9780375826689", true},
+		{"Eldest", "Christopher Paolini", "2005", "9780375840609", true},
+		{"Brisingr", "Christopher Paolini", "2008", "9780375826726", false},
+		{"Inheritance", "Christopher Paolini", "2011", "9780375846311", true},
+		{"Cobert's Bob: A Legend", "Cob Bob Bob", "1991", "8009909099-0", true}, 
+		{"Cobert's Bob: A Legend", "Cob Bob Bob", "1991", "8009909099-0", false},
+		{"Tractor's for Fun", "Rob B. Sand", "1871", "1234567890", false},
+		{"Building a House", "Dob Cob", "2014", "83199-09099-0", true},
+		{"Business Skills", "Lob Job", "2020", "8008787931-3", true},
+		{"Scientific Introduction to Trigonometry", "Snobby Toby", "1999", "7776637183", false},
+		{"Fighting Your Destiny", "Johnny Joestar", "2002", "5-34422985-0", true},
+		{"Baking for Beginners", "Chippy Chipson", "2016", "4995301122", false},
+		{"City Planning Essentials and Theory", "Don Douglas", "2010", "009-90244-31", false},
+		{"Path to Success", "Mario Mario", "2001", "599857721", false},
+		{"Dealing with Failure", "Luigi Mario", "1970", "22312544-234", false},
+		{"Counting Colors", "Steven Brown", "1963", "876654321", false},
+		{"Cobert's Bob: A Legend", "Cob Bob Bob", "1991", "8009909099-0", false},
+		{"When Will We W?: Why W is Awesome", "William Walter", "2005", "8817849-66", false},
+		{"Fighting Your Destiny", "Johnny Joestar", "2002", "5-34422985-0", false},
+		{"Eldest", "Christopher Paolini", "2005", "9780375840609", true},
 	};
 	bool working = true; 
 	int input; 
