e6608b72c2fac1069c226074747ec848b1193307
WilliamO <willyodom17@gmail.com>
2021-11-19 05:12:43 -0900

    Continued working on asciiart.cpp, completed up to accessing file and reading the header. I am unsure WHERE my file is to be read from on my computer, but otherwise should be working correctly.

--- HW7/asciiart/asciiart.cpp
+++ HW7/asciiart/asciiart.cpp
@@ -4,6 +4,8 @@ William Odom Jr
 11/18/2021
 Reads a ppm image file and creates ascii art of the image inside 
 */
+#include <sstream>
+using std::getline; 
 
 #include <fstream>
 using std::ifstream; 
@@ -15,12 +17,52 @@ using std::string;
 
 int main()
 {
-	ifstream fin("parrot.ppm");
+	//open ppm file ()
+
+	string filename = "parrot.ppm";
+	ifstream fin(filename);
 	if (!fin)
 	{
-		cout << "Error opening parrot.ppm" << endl;
+		cout << "Error opening " << filename << endl;
 		exit(1);
 	}
+	cout << "Successfully opened " << filename << endl; 
+
+	//read the header
+
+	string line; 
+	getline(fin, line);
+	if (line[0] == 'P' && line[1] == '3')
+		cout << "PPM (text) " << endl;
+	else
+	{
+		cout << "Unable to determine file type P3" << endl;
+		exit(2);
+	}
+
+	//Deal with comments at the beginning
+	getline(fin, line);
+	if (line[0] == '#')
+		cout << "Ignoring comment" << endl;
+
+	//image size and max value
+	int xres, yres, maxval; 
+	fin >> xres >> yres >> maxval; 
+	if (!fin)
+	{
+		cout << "Error reading file" << endl;
+		exit(3);
+	}
+	cout << "Image size " << xres << "x" << yres << "y" << endl;
+	cout << "Max Value = " << maxval << endl;
+
+
+
+	//read the RGB pixels
+
+	//convert to grayscale
+
+	//output each pixel as ascii characters
 
 	cout << "Done " << endl;
 }
