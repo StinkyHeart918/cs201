47dc012258d99767b65a864bb19c55e51a84061c
WilliamO <willyodom17@gmail.com>
2021-11-19 07:36:35 -0900

    Rewrote asciiart.cpp to use a function and condense and improve readibility.  Hopefully correctly to this point.

--- HW7/asciiart/asciiart.cpp
+++ HW7/asciiart/asciiart.cpp
@@ -15,64 +15,67 @@ using std::cout;
 using std::endl; 
 using std::string; 
 
-int main()
-{
-	//open ppm file ()
+#include <vector>
+using std::vector; 
 
-	string filename = "parrot.ppm";
+//Universal for our image 
+int xres, yres, maxval; 
+vector<int> ppm;
+
+bool read_ppm(string filename)
+{
+	//open file
 	ifstream fin(filename);
 	if (!fin)
-	{
-		cout << "Error opening " << filename << endl;
-		exit(1);
-	}
-	cout << "Successfully opened " << filename << endl; 
+		return false; 
 
-	//read the header
-
-	string line; 
+	//read header
+	string line;
 	getline(fin, line);
+	if (!fin)
+		return false; 
 	if (line[0] == 'P' && line[1] == '3')
 		cout << "PPM (text) " << endl;
 	else
 	{
 		cout << "Unable to determine file type P3" << endl;
-		exit(2);
+		return false;
 	}
 
 	//Deal with comments at the beginning
 	getline(fin, line);
+	if (!fin)
+		return false; 
 	if (line[0] == '#')
 		cout << "Ignoring comment" << endl;
 
 	//image size and max value
-	int xres, yres, maxval; 
-	fin >> xres >> yres >> maxval; 
+	fin >> xres >> yres >> maxval;
 	if (!fin)
 	{
-		cout << "Error reading file" << endl;
-		exit(3);
+		cout << "Error reading file resolution" << endl;
+		return false;
 	}
 
 	if (xres < 0)
 	{
-		cout << "Error within file: xres" << endl; 
-		exit(4);
+		cout << "Error within file: xres" << endl;
+		return false;
 	}
 	if (yres < 0)
 	{
 		cout << "Error within file: yres" << endl;
-		exit(4);
+		return false;
 	}
 	if (maxval < 0)
 	{
 		cout << "Error within file: Max Value" << endl;
-		exit(4);
+		return false;
 	}
 	cout << "Image size " << xres << "x" << yres << "y" << endl;
 	cout << "Max Value = " << maxval << endl;
 
-	//read the RGB pixels
+	//read rgb pixels
 	int r, g, b, y;
 	int pixels2read = xres * yres; //how many do we need in the grid? 
 	for (int i = 0; i < pixels2read; i++)
@@ -81,21 +84,56 @@ int main()
 		if (!fin)
 		{
 			cout << "Error reading pixels" << endl;
-			exit(3);
+			return false;
 		}
-		y = 0.2126 * r + 0.7152 * g + 0.0722 * b; 
-		cout << i << " = " << r << " " << g << " " << b;
+		ppm.push_back(r);
+		ppm.push_back(g);
+		ppm.push_back(b);
+	}
+	//if no issues, then note file is open and necessary info passed
+	return true; 
+}
+
+
+
+
+int main()
+{
+	//open ppm file ()
+	string filename = "parrot.ppm";
+	if (!read_ppm(filename))
+	{
+		cout << "Error opening or reading" << filename << endl;
+		exit(1);
+	}
+	cout << "Successfully opened and read file " << filename << endl; 
+
+	for (int i = 0; i < xres * yres * 3; i++)
+	{
+		int y;
+
+		//map pixel r,g,b values to luminance values
+
+		y = 0.2126 * ppm[i] + 0.7152 * ppm[i+1] + 0.0722 * ppm[i+2];
+		//cout << i << " = " << r << " " << g << " " << b;
 		cout << " -> " << y;
 		if (y < 0 || y >255)
 		{
-			cout << "Y out of range" << endl;
+			cout << "y out of range" << endl;
 			exit(5);
 		}
+
+		//map luminance to an ascii character
+		const char value[] = " .-+*@0#"; //becomes " .-+:^*!/($&@0%#" with final submission for 0-15 index
+		int y2ascii = y / 32;  // divide by 16 for final submission
+		cout << " -> " << y2ascii << endl;
+
+		//take shape
+		cout << value[y2ascii];
+		if (i % xres == xres-1)
+			cout << endl; 
+
 	}
-
-	//convert to grayscale
-
-	//output each pixel as ascii characters
 
 	cout << "Done " << endl;
 }
